ef631a94672f6df079e8fc8ffb7626ce3895851f
Tien Tran <tien@transfamily.info>
2022-01-14 16:09:47 +1100

    Implement out-of-the-box shared data between users via bindmount

---
+++ rootfs/etc/sftp.d/bindmount.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# File mounted as: /etc/sftp.d/bindmount.sh
+# Just an example (make your own)
+
+[[ -d /opt/shared ]] || exit 0
+
+function bindmount() {
+	[[ -e "$2" ]] && echo Target mount "$2" exists. Ignored. && return
+	if [ -d "$1" ]; then
+		mkdir -p "$2"
+	else
+		touch "$2"
+	fi
+    mount --bind $3 "$1" "$2"
+}
+
+# Remember permissions, you may have to fix them:
+# chown -R :users /data/common
+
+for home in `find "/home" -maxdepth 1 -mindepth 1 -type d`; do
+	for f in `find "/opt/shared" -maxdepth 1 -mindepth 1`; do
+		bindmount $f $home/`basename $f`
+	done
+done
